7403f9a91bb708285beeee5eeca3772a37618e50
Dulmini Dissanayake <53858570+DulminiD@users.noreply.github.com>
2022-11-26 11:03:59 -0500

    Update test.py

--- Acoustic/test.py
+++ Acoustic/test.py
@@ -1,11 +1,28 @@
 
+
 # import required module
 from playsound import playsound
 import time
+import random
+from mutagen.mp3 import MP3
 
 # for playing note.wav file
+audio = MP3("/home/mcpslab/Downloads/beep-01a.mp3")
+print(audio.info.length)
+output_name = 'transmitter_bits' + str(time.ctime())
 
-for i in range (10):
-    playsound('/home/mcpslab/Downloads/beep-01a.mp3')
-    print('playing sound using  playsound')
-    time.sleep(1/25)
+randomlist = []
+for i in range(0,64):
+    n = random.randint(0,1)
+    randomlist.append(n)
+print(randomlist)
+print(len(randomlist))
+
+with open(output_name + ".txt", mode='w') as file:
+    file.write(randomlist)
+
+for i in randomlist:
+    if i == 1:
+        playsound('/home/mcpslab/Downloads/beep-01a.mp3')
+    else:
+        time.sleep(0.55)
